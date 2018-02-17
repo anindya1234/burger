91e815e81c3067dc0c982e275ae6782a7f24e30b
anindya1234 <anindyagupta@email.arizona.edu>
2018-02-17 07:54:02 -0700

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Farmhouse');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno', true);
